b59fa351993a61216469ba6cee2a7074068c32e4
Santosh Singh <mail1906@gmail.com>
2016-10-11 19:50:57 +0530

    Local/Live Config

--- config.php
+++ config.php
@@ -1,11 +1,26 @@
 <?php
-$config['host'] = "localhost";
-$config['dbname'] = "santosh_docapp";
-$config['dbusername'] = "santosh_docapp";
-$config['dbuserpass'] = "#docapp1";
-$config['AppTitle'] = "B&J";
-$config['fromEmail'] = "support@justwebapp.com";
-$config['BccEmail'] = "mail1906@gmail.com";
-define('API_ACCESS_KEY', 'AIzaSyB_GxsAIMYLFGQ482-DS1h0nr3IU-uDtK4');
+$host = $_SERVER['HTTP_HOST'];
+if($host == "localhost" || $host == "")
+{
+	$config['host'] = "localhost";
+	$config['dbname'] = "santosh_docapp";
+	$config['dbusername'] = "santosh_docapp";
+	$config['dbuserpass'] = "#docapp1";
+	$config['AppTitle'] = "B&J";
+	$config['fromEmail'] = "support@justwebapp.com";
+	$config['BccEmail'] = "mail1906@gmail.com";
+}
+else
+{
+	$config['host'] = "localhost";
+	$config['dbname'] = "santosh_docapp";
+	$config['dbusername'] = "santosh_docapp";
+	$config['dbuserpass'] = "#docapp1";
+	$config['AppTitle'] = "Orthopod Online";
+	$config['fromEmail'] = "support@orthopodonline.com";
+	$config['BccEmail'] = "mail1906@gmail.com";
+}
+
+define('API_ACCESS_KEY', 'AIzaSyDzy_o1UMZ8kkz3kwWbtOWWDt1Z_yI8v6g');
 define('PAYMENT_VALIDITY', '7');
 ?>
